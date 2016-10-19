b37321b48be5197f0be10c62af8212b8ebe296af
sunbeamChen <sunbeamhome@163.com>
2016-10-19 11:46:36 +0800

    add contants header file & init version 0.1.3
    Signed-off-by: sunbeamChen <sunbeamhome@163.com>

--- SunbeamBLECentralService/Classes/SunbeamBLECentralService.h
+++ SunbeamBLECentralService/Classes/SunbeamBLECentralService.h
@@ -12,3 +12,5 @@ FOUNDATION_EXPORT const unsigned char SunbeamBLECentralServiceVersionString[];
 
 // 蓝牙中心设备管理器，提供扫描、连接、回调等一系列方法操作
 #import "SunbeamBLECentralManager.h"
+// 错误码定义
+#import "SunbeamBLECentralConstants.h"
